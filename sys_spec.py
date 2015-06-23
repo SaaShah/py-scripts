7b2541b3478e95603f46865b26388719d9e87500
Saad Bin Shahid <saadbinshahid.fb@gmail.com>
2015-06-23 14:14:01 +0500

    sys_spec.py added.
    A python script to get system basic specification.
    +----------------------------------------------------------------------------+
       usage :: python /your/script/dir/sys_spec.py
    +----------------------------------------------------------------------------+

---
+++ sys_spec.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+os.system("clear")
+
+print "YOUR SYSTEM SPECIFICATION \n"
+
+print "USERNAME" 
+os.system("uname -n \n")
+
+print "\nKERNEL "
+os.system("uname -s")
+
+print "\nVERSION "
+os.system("uname -r")
+
+print "\nOS "
+os.system("uname -o")
+
+print "\nHARDWARE "
+os.system("uname -m")
+
+print "\nPROCESSOR "
+os.system("uname -p")
+
+print "\nPLATFORM "
+os.system("uname -i")
+
+print '\n'
